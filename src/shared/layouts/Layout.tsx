bb75f772f7fd1429e722b9bb180ab70d122e1b32
Jesus Martin <jesus8.mb@gmail.com>
2025-10-27 17:34:25 +0100

    typexcript configuration

--- src/shared/layouts/Layout.tsx
+++ src/shared/layouts/Layout.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const Layout = () => {
+const Layout: React.FC = () => {
   return(
   <>
-    <h1>Hello world from Layout</h1>
+    <h1 className='headline-XL-bold text-primary-700'>Hello world from Layout</h1>
+    <h1 className='headline-L-regular text-primary-700'>Hello world from Layout</h1>
+    <h1 className='headline-M-bold text-primary-700'>Hello world from Layout</h1>
   </>
 )}
 
